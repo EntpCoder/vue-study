10acede5efee13989ee09fed3315b6903c374452
Yang Song <entpcoder@gmail.com>
2022-10-26 09:19:40 +0800

    作用域插槽

---
+++ vue_test_cli/22_插槽/3_src_作用域插槽/main.js
@@ -0,0 +1,14 @@
+// 引入vue
+import Vue from 'vue'
+// 引入App
+import App from './App.vue'
+// 关闭生产提示
+Vue.config.productionTip = false
+// 创建vm
+new Vue({
+    render: h=>h(App),
+    beforeCreate(){
+        // 全局事件总线
+        Vue.prototype.$bus = this
+    }
+}).$mount('#app')
